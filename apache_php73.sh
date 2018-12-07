0c9d9ae06c5ca97336069553ffe8288ee1a6a166
林 <hayashi@site-lab.jp>
2018-12-07 12:53:01 +0900

    centosユーザー追加

--- apache_php73.sh
+++ apache_php73.sh
@@ -500,6 +500,28 @@ echo '<?php phpinfo(); ?>' >> /var/www/html/info.php
 cat /var/www/html/info.php
 end_message
 
+#ユーザー作成
+start_message
+echo "centosユーザーを作成します"
+USERNAME='centos'
+PASSWORD=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 10 | head -1)
+
+useradd -m -G apache -s /bin/bash "${USERNAME}"
+echo "${PASSWORD}" | passwd --stdin "${USERNAME}"
+echo "パスワードは"${PASSWORD}"です。"
+
+#所属グループ表示
+echo "所属グループを表示します"
+getent group apache
+end_message
+
+#所有者の変更
+start_message
+echo "ドキュメントルートの所有者をcentos、グループをapacheにします"
+chown -R centos:apache /var/www/html
+end_message
+
+
 # apacheの起動
 echo "apacheを起動します"
 start_message
@@ -531,6 +553,8 @@ echo "設定を表示"
 echo ""
 firewall-cmd --list-all
 end_message
+
+umask 0002
 
 cat <<EOF
 http://IPアドレス/info.php
@@ -576,7 +600,9 @@ https://www.logw.jp/server/8359.html
 </VirtualHost>
 
 
-これにて終了です
-
-ドキュメントルートの所有者：グループは｢root｣になっているため、ユーザー名とグループを変更してください
+ドキュメントルートの所有者：centos
+グループ：apache
+になっているため、ユーザー名とグループの変更が必要な場合は変更してください
 EOF
+echo "centosユーザーのパスワードは"${PASSWORD}"です。"
+exec $SHELL -l
