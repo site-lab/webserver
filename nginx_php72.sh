b8b13b06016fd69cdf80e402adfb76b4fdc74d79
林 <hayashi@site-lab.jp>
2018-12-08 15:19:00 +0900

    centosユーザー追加

--- nginx_php72.sh
+++ nginx_php72.sh
@@ -7,11 +7,13 @@
 URL：https://www.site-lab.jp/
 URL：https://www.logw.jp/
 
-注意点：conohaのポートは全て許可前提となります。もしくは80番、443番の許可をしておいてください。システムのfirewallはオン状態となります
+注意点：conohaのポートは全て許可前提となります。もしくは80番、443番の許可をしておいてください。システムのfirewallはオン状態となります。centosユーザーのパスワードはランダム生成となります。最後に表示されます
 
 目的：システム更新+nginxのインストール
 ・nginx
 ・mod_sslのインストール
+・php7系のインストール
+・centosユーザーの作成
 
 COMMENT
 
@@ -709,6 +711,26 @@ echo '<?php phpinfo(); ?>' >> /usr/share/nginx/html/info.php
 cat /usr/share/nginx/html/info.php
 end_message
 
+ユーザー作成
+start_message
+echo "centosユーザーを作成します"
+USERNAME='centos'
+PASSWORD=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 10 | head -1)
+
+useradd -m -G apache -s /bin/bash "${USERNAME}"
+echo "${PASSWORD}" | passwd --stdin "${USERNAME}"
+echo "パスワードは"${PASSWORD}"です。"
+
+#所属グループ表示
+echo "所属グループを表示します"
+getent group nginx
+end_message
+
+#所有者の変更
+start_message
+echo "ドキュメントルートの所有者をcentos、グループをapacheにします"
+chown -R centos:nginx /usr/share/nginx/html
+end_message
 
 #php-fpmの起動
 start_message
@@ -768,5 +790,10 @@ httpsリダイレクトについて
 コメントを外せばそのままリダイレクトになります。
 ---------------------------------------
 
-ドキュメントルートの所有者：グループは｢root｣になっているため、ユーザー名とグループを変更してください
+ドキュメントルートの所有者：centos
+グループ：nginx
+になっているため、ユーザー名とグループの変更が必要な場合は変更してください
 EOF
+
+echo "centosユーザーのパスワードは"${PASSWORD}"です。"
+exec $SHELL -l
