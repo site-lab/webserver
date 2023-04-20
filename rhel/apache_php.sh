2772c0576ad95f909c12dd662d89fdfb65f62f39
hayashin10@gmail.com <hayashin10@gmail.com>
2023-04-20 19:28:51 +0900

    yesを追加

--- rhel/apache_php.sh
+++ rhel/apache_php.sh
@@ -70,7 +70,7 @@ if [ -e /etc/redhat-release ]; then
 
         #必要な拡張モジュールをインストール
         echo "モジュールのインストール"
-        dnf install php-cli php-fpm php-curl php-mysqlnd php-gd php-opcache php-zip php-intl php-common php-bcmath php-imagick php-xmlrpc php-json php-readline php-memcached php-redis php-mbstring php-apcu php-xml php-dom php-redis php-memcached php-memcache
+        dnf install -y php-cli php-fpm php-curl php-mysqlnd php-gd php-opcache php-zip php-intl php-common php-bcmath php-imagick php-xmlrpc php-json php-readline php-memcached php-redis php-mbstring php-apcu php-xml php-dom php-redis php-memcached php-memcache
         end_message
 
 
